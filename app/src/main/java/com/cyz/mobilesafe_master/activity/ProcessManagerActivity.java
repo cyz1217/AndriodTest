c318a292cd904c421e5247192b9b3cebd9d88522
cyz <1743935569@qq.com>
2022-05-16 16:23:11 +0800

    陈云泽，进程管理界面

---
+++ app/src/main/java/com/cyz/mobilesafe_master/activity/ProcessManagerActivity.java
@@ -0,0 +1,17 @@
+package com.cyz.mobilesafe_master.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.cyz.mobilesafe_master.R;
+
+public class ProcessManagerActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_manager);
+
+    }
+}
